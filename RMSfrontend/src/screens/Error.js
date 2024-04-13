c2fc5a6bd379be1378891a436734a914ef296e9e
Malik Umar <umargulzar2001@gmail.com>
2024-04-14 00:07:35 +0500

    created a dummy error page

---
+++ RMSfrontend/src/screens/Error.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import defaultStyles from "../defaultStyles";
+
+const Error = () => {
+  return (
+    <View>
+      <View
+        style={[defaultStyles.container, { backgroundColor: "transparent" }]}
+      >
+        <Text style={[defaultStyles.fWB, defaultStyles.fs25]}>Error....</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Error;
